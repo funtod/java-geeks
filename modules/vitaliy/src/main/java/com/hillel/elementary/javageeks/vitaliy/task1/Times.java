f33bf81d9037ef6775b708ff1784bdf6d139f72b
Vitaliy <v.v.grygorovych@gmail.com>
2018-10-21 18:36:48 +0300

    added task1

---
+++ modules/vitaliy/src/main/java/com/hillel/elementary/javageeks/vitaliy/task1/Times.java
@@ -0,0 +1,13 @@
+package com.hillel.elementary.javageeks.vitaliy.task1;
+
+import java.util.Date;
+
+public class Times {
+    public static void main(String args[]) {
+        // Инициализация объекта date
+        Date date = new Date();
+
+        // Вывод текущей даты и времени с использованием toString()
+        System.out.println("\n" + date.toString());
+    }
+}

edd471887db3aee4cbbb85bc96f83d5a5c2db21d
Zhurylo Mikhaylo <dir2000@yjr.net>
2018-10-21 17:36:33 +0300

    deleted extra comments

--- modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/TwoIntegersTaskOne.java
+++ modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/TwoIntegersTaskOne.java
@@ -123,15 +123,3 @@ public class TwoIntegersTaskOne extends JFrame {
         }
     }
 }
-
-/*Test Data
-Input 1st integer: 25
-Input 2nd integer: 5
-Expected Output :
-Sum of two integers: 30
-Difference of two integers: 20
-Product of two integers: 125
-Average of two integers: 15.00
-Distance of two integers: 20
-Max integer: 25
-Min integer: 5*/

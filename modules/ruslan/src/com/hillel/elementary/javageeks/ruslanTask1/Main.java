3c20a934e6ec67e206b367a6f18e5d212e49be20
Ruslan <rstepanchenko@gmail.com>
2018-10-22 15:48:09 +0300

    was added Task1 Ruslan

---
+++ modules/ruslan/src/com/hillel/elementary/javageeks/ruslanTask1/Main.java
@@ -0,0 +1,46 @@
+package com.hillel.elementary.javageeks.ruslanTask1;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        do {
+            System.out.println("Please enter number from 1 to 7");
+            while (!sc.hasNextInt()) {//check before int value will be input
+                System.out.println("Please enter number from 1 to 7");
+                sc.next();//promt value
+            }
+            number = sc.nextInt();
+        } while (number <= 0);//check positive value
+        switch(number){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("test");
+                //  Calendar aDate = new GregorianCalendar(y, (m - 1), d);
+                //  int dayOfWeek = aDate.get(Calendar.DAY_OF_WEEK);
+                //  System.out.println(dayOfWeek);
+                sc.close();
+        }
+    }
+}

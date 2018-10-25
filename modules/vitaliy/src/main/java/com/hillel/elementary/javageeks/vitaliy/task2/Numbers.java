7b7f6b05fa3101728838e6931ec731ed5f3f95c3
Vitaliy <v.v.grygorovych@gmail.com>
2018-10-25 19:26:32 +0300

    deleted file .nano

--- modules/vitaliy/src/main/java/com/hillel/elementary/javageeks/vitaliy/task2/Numbers.java
+++ modules/vitaliy/src/main/java/com/hillel/elementary/javageeks/vitaliy/task2/Numbers.java
@@ -9,23 +9,23 @@ public class Numbers {
     public static void main(String[] args) {
         System.out.println("Введите число - как много переменных вы хотите добавить в массив и нажмите <Enter>:");
         Scanner scan = new Scanner(System.in);
-        int chisloNaVhode = scan.nextInt();
-        int massiv[] = new int[chisloNaVhode];
-        int numbers[] = new int[chisloNaVhode];
+        int numberIn = scan.nextInt();
+        int massive[] = new int[numberIn];
+        int numbers[] = new int[numberIn];
         System.out.println();
         System.out.println("Сейчас введите ваши числа через пробел и нажмите <Enter>:");
-        for (int i = 0; i < chisloNaVhode; i++) {
-            massiv[i] = scan.nextInt();
-            numbers[i] = (massiv[i] + "").length();
+        for (int i = 0; i < numberIn; i++) {
+            massive[i] = scan.nextInt();
+            numbers[i] = (massive[i] + "").length();
         }
         scan.close();
-        numbersLength(massiv, numbers, chisloNaVhode);
+        numbersLength(massive, numbers, numberIn);
 
     }
 
-    private static void numbersLength(int[] massiv, int[] numbers, int chisloNaVhode) {
+    private static void numbersLength(int[] massive, int[] numbers, int numberIn) {
         int max = numbers[0], min = numbers[0], big = 0, small = 0;
-        for (int i = 1; i < chisloNaVhode; i++) {
+        for (int i = 1; i < numberIn; i++) {
             int j = numbers[i];
             if (min > j) {
                 min = j;
@@ -40,7 +40,7 @@ public class Numbers {
         System.out.println();
         System.out.println("*********************************");
         System.out.println();
-        System.out.println("самое короткое число: " + massiv[small] + "." + " Его длина: " + min + ";");
-        System.out.println("самое длинное число: " + massiv[big] + "." + " Его длина: " + max + ";");
+        System.out.println("самое короткое число: " + massive[small] + "." + " Его длина: " + min + ";");
+        System.out.println("самое длинное число: " + massive[big] + "." + " Его длина: " + max + ";");
     }
 }

514138aae54ed2018d8f08cc58ce69f4774ed075
Alex <nlvk48@gmail.com>
2018-10-25 12:39:39 +0300

    major bug fix

--- modules/alex-n/src/main/java/com/hillel/elementary/javageeks/cellular_automaton/Main.java
+++ modules/alex-n/src/main/java/com/hillel/elementary/javageeks/cellular_automaton/Main.java
@@ -26,7 +26,7 @@ public class Main {
             System.exit(0);
         }
 
-        int[][] matrix = new int[rows][columns];
+        int[][] matrix = new int[columns][rows];
 
         matrix = fillMatrix(matrix);
         System.out.println("Initialized matrix:");
@@ -44,9 +44,9 @@ class MatrixProcessor{
     //fill matrix with random 0 or 1
     public static int[][] fillMatrix(int[][] matrix){
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[i][j] = (int) Math.round(Math.random());
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[j][i] = (int) Math.round(Math.random());
             }
         }
         return matrix;
@@ -57,54 +57,54 @@ class MatrixProcessor{
 
         int columnsNumber = matrix.length;
         int rowsNumber = matrix[0].length;
-        int[][] afterOneCycleMatrix = new int[rowsNumber][columnsNumber];
+        int[][] afterOneCycleMatrix = new int[columnsNumber][rowsNumber];
 
-        for (int j = 0; j < matrix.length; j++) {
-            for (int i = 0; i < matrix[0].length; i++) {
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
                 int neighbours = 0;
 
-                if (i > 0 && j > 0) {                            //upper left corner
-                    neighbours += matrix[i - 1][j - 1];
+                if (j > 0 && i > 0) {                            //upper left corner
+                    neighbours += matrix[j - 1][i - 1];
                 }
 
-                if (j > 0) {                                     //upper edge
-                    neighbours += matrix[i][j - 1];
+                if (i > 0) {                                     //upper edge
+                    neighbours += matrix[j][i - 1];
                 }
 
-                if (i < columnsNumber - 1 && j > 0) {            //upper right corner
-                    neighbours += matrix[i + 1][j - 1];
+                if (j < columnsNumber - 1 && i > 0) {            //upper right corner
+                    neighbours += matrix[j + 1][i - 1];
                 }
 
-                if (i > 0) {                                     //left edge
-                    neighbours += matrix[i - 1][j];
+                if (j > 0) {                                     //left edge
+                    neighbours += matrix[j - 1][i];
                 }
 
-                if (i < columnsNumber - 1) {                     //right edge
-                    neighbours += matrix[i + 1][j];
+                if (j < columnsNumber - 1) {                     //right edge
+                    neighbours += matrix[j + 1][i];
                 }
 
-                if (i > 0 && j < rowsNumber - 1) {               //bottom left corner
-                    neighbours += matrix[i - 1][j + 1];
+                if (j > 0 && i < rowsNumber - 1) {               //bottom left corner
+                    neighbours += matrix[j - 1][i + 1];
                 }
 
-                if (j < rowsNumber - 1) {                        //bottom edge
-                    neighbours += matrix[i][j + 1];
+                if (i < rowsNumber - 1) {                        //bottom edge
+                    neighbours += matrix[j][i + 1];
                 }
 
-                if (i < columnsNumber - 1 && j < rowsNumber - 1) { //bottom right corner
-                    neighbours += matrix[i+1][j+1];
+                if (j < columnsNumber - 1 && i < rowsNumber - 1) { //bottom right corner
+                    neighbours += matrix[j+1][i+1];
                 }
 
 
-                if (matrix[i][j]==1){
+                if (matrix[j][i]==1){
                     if (neighbours > 3 || neighbours < 2){
-                        afterOneCycleMatrix[i][j] = 0;
+                        afterOneCycleMatrix[j][i] = 0;
                     }else {
-                        afterOneCycleMatrix[i][j] = 1;
+                        afterOneCycleMatrix[j][i] = 1;
                     }
                 }else {
                     if(neighbours == 3){
-                        afterOneCycleMatrix[i][j] = 1;
+                        afterOneCycleMatrix[j][i] = 1;
                     }
                 }
             }
@@ -115,9 +115,9 @@ class MatrixProcessor{
 
     public static void printMatrix(int[][] matrix){
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.printf(String.valueOf(matrix[i][j]) + " ");
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.printf(String.valueOf(matrix[j][i]) + " ");
             }
             System.out.printf("\n");
         }

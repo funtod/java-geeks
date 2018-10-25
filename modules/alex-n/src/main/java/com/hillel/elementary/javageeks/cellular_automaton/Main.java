7d14d4324515a9fd4d5b48cb9263cf084519f505
Alex <nlvk48@gmail.com>
2018-10-25 14:55:41 +0300

    fix OutOfBoundsException

--- modules/alex-n/src/main/java/com/hillel/elementary/javageeks/cellular_automaton/Main.java
+++ modules/alex-n/src/main/java/com/hillel/elementary/javageeks/cellular_automaton/Main.java
@@ -10,20 +10,29 @@ public class Main {
         int rows = 0;
         int columns = 0;
         Scanner scanner = new Scanner(System.in);
+        boolean initializingData = true;
 
-        System.out.printf("please enter number of columns: ");
-        if (scanner.hasNextInt()) {
-            columns = scanner.nextInt();
-        }else {
-            System.out.printf("wrong data. Exit");
-            System.exit(0);
-        }
-        System.out.printf("Please enter number of rows: ");
-        if (scanner.hasNextInt()) {
-            rows = scanner.nextInt();
-        }else {
-            System.out.printf("wrong data. Exit");
-            System.exit(0);
+        while (initializingData) {
+            System.out.printf("Please enter number of columns: ");
+            if (scanner.hasNextInt()) {
+                columns = scanner.nextInt();
+            } else {
+                System.out.printf("wrong data. Exit");
+                System.exit(0);
+            }
+            System.out.printf("Please enter number of rows: ");
+            if (scanner.hasNextInt()) {
+                rows = scanner.nextInt();
+            } else {
+                System.out.printf("wrong data. Exit");
+                System.exit(0);
+            }
+            if (columns <= 0 || rows <= 0){
+                System.out.println("Error. Can't sat matrix: " + columns + " x " + rows + ". Dimensions must be grater than 0. Try again.");
+                initializingData = true;
+            }else{
+                initializingData = false;
+            }
         }
 
         int[][] matrix = new int[columns][rows];

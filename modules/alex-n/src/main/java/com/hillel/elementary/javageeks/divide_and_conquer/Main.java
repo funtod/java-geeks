71d085adf22aa779332efb12c7f3a96dd8c64df6
Alex <nlvk48@gmail.com>
2018-10-28 03:04:02 +0300

    divide and conquer add tests and user input

--- modules/alex-n/src/main/java/com/hillel/elementary/javageeks/divide_and_conquer/Main.java
+++ modules/alex-n/src/main/java/com/hillel/elementary/javageeks/divide_and_conquer/Main.java
@@ -1,11 +1,32 @@
 package com.hillel.elementary.javageeks.divide_and_conquer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        int[] unsortedArray = new int[]{4,3,2,1,4,5,7,3,12,7,84,34,5,6,7,-1};
-        unsortedArray = Sorter.mergeSort(unsortedArray);
-        for (int elem : unsortedArray){
-            System.out.printf(elem + ", ");
+    public static void main(String[] args) throws IOException {
+        int[] array;
+        String userInput;
+        System.out.println("Please input your numbers separated by commas:");
+
+        //This all just to don't ask user about array length
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        userInput = bufferedReader.readLine().trim();
+        String[] userInputArray = userInput.split(",");
+        array = new int[userInputArray.length];
+        for (int i = 0; i < userInputArray.length; i++) {
+            try {
+                array[i] = Integer.parseInt(userInputArray[i].trim());
+            }catch (NumberFormatException e){
+                System.out.println("Wrong input data! Must be integer numbers separated by commas.");
+                System.exit(1);
+            }
+        }
+
+        array = Sorter.mergeSort(array);
+        for (int elem : array) {
+            System.out.printf(elem + " ");
         }
     }
 }

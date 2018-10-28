5df674ac9bc24af4467748d4b71b3c26d0c7eb24
project <artem.hillel@gmail.com>
2018-10-28 22:06:15 +0200

    add modif

--- modules/artem/src/main/java/com/hillel/elementary/javageeks/artem/Sort.java
+++ modules/artem/src/main/java/com/hillel/elementary/javageeks/artem/Sort.java
@@ -1,4 +1,36 @@
 package com.hillel.elementary.javageeks.artem;
 
+import java.util.Scanner;
+
 public class Sort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+        System.out.print("Enter size of array: ");
+        size = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println("Array:");
+        add(array);
+        print(array);
+        System.out.println("Array:");
+        print(array);
+    }
+
+    public static void add(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+    }
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void sort() {
+
+    }
+
 }

4048ae9f00a5b5a9eb50c8d0ef3c62b7c9b2b279
Ruslan <rstepanchenko@gmail.com>
2018-11-07 19:40:41 +0200

    JG-44-Homework4-Ruslan-CreatePlane

---
+++ modules/ruslan/src/main/java/com/hillel/elementary/javageeks/ruslanTask4/Engine.java
@@ -0,0 +1,35 @@
+package com.hillel.elementary.javageeks.ruslanTask4;
+
+
+public class Engine {
+
+    int count;
+    String type;
+
+    public Engine(int count, String type) {
+        this.count = count;
+        this.type = type;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return " TeslaModel_5 [count=" + count + ", type=" + type + "]";
+    }
+
+}
---
+++ modules/ruslan/src/test/java/com/hillel/elementary/javageeks/ruslanTask4/WingTest.java
@@ -0,0 +1,34 @@
+package com.hillel.elementary.javageeks.ruslanTask4;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
+public class WingTest {
+
+    public Wing wing;
+
+    @BeforeAll
+    public void shouldGetWing() {
+        wing = new Wing (4, "long and bright", "aircraft");
+    }
+
+    @Test
+    public void ShouldPrintWingModel() {
+        assertThat(wing.getName()).isEqualTo("long and bright");
+    }
+
+    @Test
+    public void ShouldPrintWingCount() {
+        assertThat(wing.getCount()).isEqualTo(4);
+    }
+
+    @Test
+    public void ShouldPrintWingManufacturer() {
+        assertThat(wing.getManufacturer()).isEqualTo("aircraft");
+    }
+
+}

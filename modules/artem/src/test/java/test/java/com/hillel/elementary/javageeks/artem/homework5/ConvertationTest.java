b79a8e0623d9a164b22ebf7a23217f1b8875002d
project <artem.hillel@gmail.com>
2018-11-14 00:34:00 +0200

    add JG-63

---
+++ modules/artem/src/test/java/test/java/com/hillel/elementary/javageeks/artem/homework5/ConvertationTest.java
@@ -0,0 +1,22 @@
+package test.java.com.hillel.elementary.javageeks.artem.homework5;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ConvertationTest {
+
+    @Test
+    public void shouldReturnConvertResultIfNotException(){
+        Convertation convertation = new Convertation();
+        String str = convertation.convertation(5);
+        assertThat(str).isEqualTo("5.0");
+    }
+
+    @Test
+    public void shouldReturnNaNIfException(){
+        Convertation convertation = new Convertation();
+        double number = Math.sqrt(-1);
+        String str = convertation.convertation(number);
+        assertThat(str).isEqualTo("NaN");
+    }
+}

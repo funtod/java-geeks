c87e9532ffaa8f77cb6721192f7cbbc0c728f1af
Zhurylo Mykhailo <dir2000@ukr.net>
2018-11-14 18:42:55 +0200

    JG-89 dir2000 version 1.0

--- modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/homework8/word_permutation/Processor.java
+++ modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/homework8/word_permutation/Processor.java
@@ -8,18 +8,34 @@ package com.hillel.elementary.javageeks.dir.homework8.word_permutation;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Processor {
+  static private final Pattern PATTERN = Pattern.compile("(.*?)(\\w+)((.*?\\s+.*?)+)(\\w+)(.*?)");
+
   public static File process(URL inputURL) throws Exception {
     File inputFile = new File(inputURL.getFile());
     File outputFile = getOutputFile(inputFile);
     try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8))) {
       try (PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8))) {
+        StringBuilder builder = new StringBuilder();
         while (true) {
           String line = reader.readLine();
           if (line == null)
             break;
-          writer.println(line);
+          Matcher matcher = PATTERN.matcher(line);
+          if (matcher.matches()){
+            builder.setLength(0);
+            builder.append(matcher.group(1));
+            builder.append(matcher.group(5));
+            builder.append(matcher.group(3));
+            builder.append(matcher.group(2));
+            builder.append(matcher.group(6));
+            writer.println(builder.toString());
+          } else {
+            writer.println(line);
+          }
         }
       }
     }
@@ -29,14 +45,16 @@ public class Processor {
 
   private static File getOutputFile(File inputFile) throws Exception {
     String inputFileName = inputFile.getName();
-    String prefix = inputFileName;
-    String suffix = "";
+    String prefix, suffix;
     int extensionPointIndex = inputFileName.lastIndexOf(".");
-    if (extensionPointIndex >= 0) {
+    if (extensionPointIndex == -1) {
+      prefix = inputFileName;
+      suffix = "";
+    } else{
       prefix = inputFileName.substring(0, extensionPointIndex);
       suffix = inputFileName.substring(extensionPointIndex);
     }
-    prefix += "_swapped";
+    suffix = "_swapped" + suffix;
 
     return File.createTempFile(prefix, suffix);
   }
--- modules/dir2000/src/test/java/com/hillel/elementary/javageeks/dir/homework8/word_permutation/ProcessorTest.java
+++ modules/dir2000/src/test/java/com/hillel/elementary/javageeks/dir/homework8/word_permutation/ProcessorTest.java
@@ -4,19 +4,41 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProcessorTest {
 
   @Test
-  void process()  throws Exception{
+  void process() throws Exception {
     //given
     URL inputURL = getClass().getClassLoader().getResource("1074-0.txt");
+    URL expectedFileURL = getClass().getClassLoader().getResource("1074-0_swapped.txt");
+    String expectedFileHash = getFileHash(Paths.get(expectedFileURL.toURI()).toFile());
 
     //when
     File outputFile = Processor.process(inputURL);
+    String outputFileHash = getFileHash(outputFile);
 
-    System.out.println(outputFile.getCanonicalPath());
+    //then
+    assertEquals(outputFileHash, expectedFileHash);
+  }
+
+  private String getFileHash(File file) throws Exception {
+    MessageDigest alg = MessageDigest.getInstance("SHA-256");
+    byte[] input = Files.readAllBytes(file.toPath());
+    byte[] hash = alg.digest(input);
+
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < hash.length; i++) {
+      int v = hash[i] & 0xFF;
+      if (v < 16) builder.append("0");
+      builder.append(Integer.toString(v, 16).toUpperCase());
+      builder.append(" ");
+    }
+    return builder.toString();
   }
 }

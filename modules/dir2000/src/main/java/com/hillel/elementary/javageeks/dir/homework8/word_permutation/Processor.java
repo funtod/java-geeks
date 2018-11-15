545574ef6bd16f8fc08299ebefb452e3b9987894
Zhurylo Mykhailo <dir2000@ukr.net>
2018-11-15 13:05:14 +0200

    JG-89 dir2000 style check has been taken into account

--- modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/homework8/word_permutation/Processor.java
+++ modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/homework8/word_permutation/Processor.java
@@ -11,27 +11,40 @@ import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Processor {
-  static private final Pattern PATTERN = Pattern.compile("(.*?)(\\w+)((.*?\\s+.*?)+)(\\w+)(.*?)");
+//checkstyle: Class Processor should be declared as final
+public final class Processor {
+  //checkstyle: Utility classes should not have a public or default constructor.
+  private Processor() {
+  }
+
+  private static final Pattern PATTERN = Pattern.compile("(.*?)(\\w+)((.*?\\s+.*?)+)(\\w+)(.*?)");
 
   public static File process(URL inputURL) throws Exception {
     File inputFile = new File(inputURL.getFile());
     File outputFile = getOutputFile(inputFile);
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8))) {
-      try (PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8))) {
+    try (BufferedReader reader =
+                 new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8))) {
+      try (PrintWriter writer =
+                   new PrintWriter(new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8))) {
         StringBuilder builder = new StringBuilder();
+        final int beginGroup = 1;
+        final int firstWordGroup = 2;
+        final int innerGroup = 3;
+        final int lastWordGroup = 5;
+        final int endGroup = 6;
         while (true) {
           String line = reader.readLine();
-          if (line == null)
+          if (line == null) {
             break;
+          }
           Matcher matcher = PATTERN.matcher(line);
-          if (matcher.matches()){
+          if (matcher.matches()) {
             builder.setLength(0);
-            builder.append(matcher.group(1));
-            builder.append(matcher.group(5));
-            builder.append(matcher.group(3));
-            builder.append(matcher.group(2));
-            builder.append(matcher.group(6));
+            builder.append(matcher.group(beginGroup));
+            builder.append(matcher.group(lastWordGroup));
+            builder.append(matcher.group(innerGroup));
+            builder.append(matcher.group(firstWordGroup));
+            builder.append(matcher.group(endGroup));
             writer.println(builder.toString());
           } else {
             writer.println(line);
@@ -50,7 +63,7 @@ public class Processor {
     if (extensionPointIndex == -1) {
       prefix = inputFileName;
       suffix = "";
-    } else{
+    } else {
       prefix = inputFileName.substring(0, extensionPointIndex);
       suffix = inputFileName.substring(extensionPointIndex);
     }

19374f5d9f3e3c980f22b27399573edced2c04f5
Ruslan <rstepanchenko@gmail.com>
2018-11-26 11:28:56 +0200

    test ModifyFile

--- modules/ruslan/src/main/java/com/hillel/elementary/javageeks/ruslanTask6/ReadFileChangeFirstLetter.java
+++ modules/ruslan/src/main/java/com/hillel/elementary/javageeks/ruslanTask6/ReadFileChangeFirstLetter.java
@@ -5,39 +5,72 @@ import java.net.URL;
 
 public class ReadFileChangeFirstLetter {
 
-    public static void main(String[] args) throws IOException {
-        URL resource =ReadFileChangeFirstLetter.class.getClassLoader ().getResource ("Test.txt");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        URL resource =ReadFileChangeFirstLetter.class.getClassLoader ().getResource ("Test1.txt");
         String fileFrom = resource.getFile ();
  //       try {
             File filename = new File(fileFrom);
-           // filename.exists();
-            System.out.println(filename.exists());
-            FileInputStream fis = new FileInputStream(filename);//читающий поток
-            DataInputStream ods = new DataInputStream(fis);
-            int size = fis.available();
-//
-           char Array[] = new char[size];
-           for (int i = 0; i < size; i++) {
-               if (fis.read() != -1) {
-                   Array[i] = ods.readChar();
-                   System.out.println(Array[i]);
-               }
-           }
-           fis.close();
-//
-//            FileOutputStream os = new FileOutputStream("SonRec.txt");
-//            DataOutputStream od = new DataOutputStream(os);
-//            for (int i = 0; i < size; i++) {
-//                if (Array[i] == ' ') {
-//                    Array[i + 1] = Character.toUpperCase(Array[i + 1]);
-//                }
-//                od.writeChar(Array[i]);
-//            }
-//
-//            os.close();
- //       }
+         FileInputStream inputStream;
+
+        // Класс для работы потоком ввода в файл
+         FileOutputStream outputStream;
+
+                 // инициализируем поток вывода из файлу
+            inputStream = new FileInputStream(fileFrom);
+
+            // читаем первый символ с потока байтов
+            int data = inputStream.read();
+            char content;
+
+            // если data будет равна 0 то это значит,
+            // что файл пуст
+        boolean fUper=false;
+        while(data != -1) {
+            // переводим байты в символ
+            content = (char) data;
+
+            // выводим полученный символ
+            if (fUper) {
+                System.out.print(String.valueOf(content).toUpperCase());
+                fUper=false;
+            }
+            System.out.print(content);
+            if (content==' '){
+                fUper = true;
+            }
+
+
+            // читаем следующий байты символа
+            data = inputStream.read();
+        }
+
+            // закрываем поток чтения файла
+            inputStream.close();
+
+
+
 
     }
+
+
+    public static void write(String st,String fileFrom) throws IOException {
+        FileInputStream inputStream;
+
+        // Класс для работы потоком ввода в файл
+        FileOutputStream outputStream;
+        // открываем поток ввода в файл
+        outputStream = new FileOutputStream(fileFrom);
+
+        // записываем данные в файл, но
+        // пока еще данные не попадут в файл,
+        // а просто будут в памяти
+        outputStream.write(st.getBytes());
+
+        // только после закрытия потока записи,
+        // данные попадают в файл
+        outputStream.close();
+    }
+
 }
 
 

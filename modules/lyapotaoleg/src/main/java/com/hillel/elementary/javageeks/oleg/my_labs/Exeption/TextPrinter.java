2efda2cbc5de47e597efe54f27f800b76b7f8c4f
Oleg <lyapotaoleg@ukr.net>
2018-11-26 15:16:09 +0200

    Homework for exeption with corect

---
+++ modules/lyapotaoleg/src/main/java/com/hillel/elementary/javageeks/oleg/my_labs/Exeption/TextPrinter.java
@@ -0,0 +1,7 @@
+package com.hillel.elementary.javageeks.oleg.my_labs.Exeption;
+
+public class TextPrinter {
+    public static void print(String text) throws MyExeption {
+        TextConvert.convert (text);
+    }
+}

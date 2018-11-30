25cd6a5c2a96e6620f93f4e014f42037ba088814
Ruslan <rstepanchenko@gmail.com>
2018-11-30 13:52:58 +0200

    was added homework6

--- modules/ruslan/src/main/java/com/hillel/elementary/javageeks/ruslanTask6/ReadInBUF.java
+++ modules/ruslan/src/main/java/com/hillel/elementary/javageeks/ruslanTask6/ReadInBUF.java
@@ -1,4 +1,69 @@
 package com.hillel.elementary.javageeks.ruslanTask6;
 
+import java.io.*;
+
 public class ReadInBUF {
+
+    public static void writeFile(String FILENAME, String str) throws IOException {
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(new File(FILENAME)));
+        writer.append(str);
+        writer.close();
+    }
+
+    public static byte[] toLow(byte[] arr){
+
+        for (int i = 0; i< arr.length; i++) {
+            arr[i] = (byte) Character.toLowerCase(arr[i]);
+        }
+        return arr;
+    }
+
+    public static String toUp(byte[] arr){
+
+        boolean inWord = false;
+        for (int i = 0; i< arr.length; i++) {
+            if (arr[i] >= 97 && arr[i] <= 122) {
+                if (inWord) {
+                    continue;
+                }
+                arr[i] = (byte) Character.toUpperCase(arr[i]);
+                inWord = true;
+            } else {
+                inWord = false;
+            }
+        }
+        return new String(arr);
+    }
+    
+
+    public static byte[] getFileBytes(File file) throws IOException {
+
+        ByteArrayOutputStream ous = null;
+        InputStream ios = null;
+        try {
+
+            byte[] buffer = new byte[4096];
+            ous = new ByteArrayOutputStream();
+            ios = new FileInputStream(file);
+            int read = 0;
+            while ((read = ios.read(buffer)) != -1)
+                ous.write(buffer, 0, read);
+
+        } finally {
+
+            try {
+
+                if (ous != null)
+                    ous.close();
+            } catch (IOException e) {
+            }
+            try {
+                if (ios != null)
+                    ios.close();
+            } catch (IOException e) {
+            }
+        }
+        return ous.toByteArray();
+    }
 }
--- modules/ruslan/src/test/java/com/hillel/elementary/javageeks/ruslanTask6/ReadInBUFTest.java
+++ modules/ruslan/src/test/java/com/hillel/elementary/javageeks/ruslanTask6/ReadInBUFTest.java
@@ -1,5 +1,49 @@
+package com.hillel.elementary.javageeks.ruslanTask6;
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.net.URL;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.io.*;
 
 class ReadInBUFTest {
+    @Test
+    public void shouldResultReadFromFile() throws IOException {
 
+        //given
+//        URL resource = ReadInBUFTest.class.getClassLoader().getResource("Text.txt");
+//        System.out.println(resource);
+//        String fileFrom = resource.getFile();
+        String fileFrom = "/home/rstepanc/IdeaProjects/java-geeks/modules/ruslan/target/classes/Test";
+
+        byte[] arr= ReadInBUF.getFileBytes(new File(fileFrom));
+        byte[] arrLower = ReadInBUF.toLow(arr);
+        ReadInBUF.writeFile(fileFrom, ReadInBUF.toUp(arrLower));
+
+        //when
+        String sCurrentLine =null;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileFrom))) {
+            while ((sCurrentLine = br.readLine()) != null) {
+                String[] parts = sCurrentLine.split(" ");
+
+                int random = (int)(Math.random() * parts.length);
+
+                String lineOutput = "";
+                String checkResult = null;
+
+                if (sCurrentLine.length() > 0) {
+                    char letter = parts[random].substring(0,1).charAt(0);
+                    if (letter >= 65 && letter <= 90){
+                        checkResult = "true";
+                    } else {
+                        checkResult = "false";
+                    }
+                }
+                assertThat(checkResult).isEqualTo("true");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

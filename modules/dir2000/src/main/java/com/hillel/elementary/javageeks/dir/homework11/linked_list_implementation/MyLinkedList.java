8ad9d70e19604dbcd0df73f37ac1ff42e464594f
Zhurylo Mikhaylo <dir2000@yjr.net>
2018-12-02 17:36:42 +0200

    linked list implementation dir2000 add some changes

--- modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/homework11/linked_list_implementation/MyLinkedList.java
+++ modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/homework11/linked_list_implementation/MyLinkedList.java
@@ -20,16 +20,24 @@ public class MyLinkedList<T> implements List<T> {
 
   @Override
   public boolean contains(Object o) {
+    Node currentNode = head;
+    while (currentNode != null) {
+      if (o == null ? currentNode.value ==null : o.equals(currentNode.value)) {
+        return  true;
+      }
+      currentNode = currentNode.next;
+    }
     return false;
   }
 
   @Override
   public Iterator<T> iterator() {
-    return null;
+    return new NodeIterator();
   }
 
   @Override
   public Object[] toArray() {
+    Object[] array = new Object[size];
     return new Object[0];
   }
 
@@ -55,26 +63,27 @@ public class MyLinkedList<T> implements List<T> {
     Node currentNode = head;
     while (currentNode != null) {
       if (o == null ? currentNode.value ==null : o.equals(currentNode.value)) {
-        Node previousNode = currentNode.previous;
-        if (previousNode != null) {
-          previousNode.next = currentNode.next;
-        } else {
-          head = currentNode.next;
-        }
-
-        if (tail == currentNode) {
-          tail = currentNode.previous;
-        }
-
-        currentNode.previous = null;
-        currentNode.next = null;
-        size--;
+        removeNode(currentNode);
         return  true;
       }
-
       currentNode = currentNode.next;
     }
     return false;
+  }
+
+  private void removeNode(Node currentNode) {
+    Node previousNode = currentNode.previous;
+    if (previousNode != null) {
+      previousNode.next = currentNode.next;
+    } else {
+      head = currentNode.next;
+    }
+    if (tail == currentNode) {
+      tail = currentNode.next;
+    }
+    currentNode.previous = null;
+    currentNode.next = null;
+    size--;
   }
 
   @Override
@@ -84,7 +93,7 @@ public class MyLinkedList<T> implements List<T> {
       Object object = iterator.next();
       if (!contains(object)) {
         return false;
-      };
+      }
     }
     return true;
   }
@@ -168,6 +177,35 @@ public class MyLinkedList<T> implements List<T> {
       previous = argPrevious;
       next = argNext;
       value = argValue;
+    }
+  }
+
+  private class NodeIterator implements Iterator {
+    private Node currentNode;
+
+    NodeIterator() {
+      this.currentNode = head;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return currentNode != null && currentNode.next != null;
+    }
+
+    @Override
+    public Object next() {
+      if (!hasNext()) {
+        throw new NoSuchElementException();
+      }
+      currentNode = currentNode.next;
+      return currentNode;
+    }
+
+    @Override
+    public void remove() {
+      Node nextNode = currentNode.next;
+      removeNode(currentNode);
+      currentNode = nextNode;
     }
   }
 }

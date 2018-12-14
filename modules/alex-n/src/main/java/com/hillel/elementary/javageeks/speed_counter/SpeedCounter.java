418afa59700ff6df4337d8a5923a2486564e0b83
Alex <nlvk48@gmail.com>
2018-12-14 20:10:15 +0200

    JG-23 refactoring

--- modules/alex-n/src/main/java/com/hillel/elementary/javageeks/speed_counter/SpeedCounter.java
+++ modules/alex-n/src/main/java/com/hillel/elementary/javageeks/speed_counter/SpeedCounter.java
@@ -1,26 +1,33 @@
 package com.hillel.elementary.javageeks.speed_counter;
 
-import javax.swing.*;
-
 public final class SpeedCounter {
-
-    private SpeedCounter() { }
 
     private static final int SECONDS_IN_HOUR = 3600;
     private static final int SECONDS_IN_MINUTE = 60;
+    private static final double MET_PER_SEC_TO_KM_PER_H_COEFFICIENT = 3.6;
+    private static final double MET_PER_SEC_TO_MIL_PER_H_COEFFICIENT = 2.237;
 
-    public static void runGui() {
-        SpeedCounterGUI scg = new SpeedCounterGUI();
-        JFrame frame = new JFrame();
-        frame.setContentPane(scg.getBasePanel());
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(scg.getBasePanel().getSize());
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    private int distance;
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    SpeedCounter(int distance, int hours, int minutes, int seconds) {
+        this.distance = distance;
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
     }
 
-    public static float countSpeedInMetersPerSecond(int meters, int hours, int minutes, int seconds) {
-        return  (float) meters / (hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTE + seconds);
+    double getSpeedInMetersPerSecond() {
+        return (double) distance / (hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTE + seconds);
+    }
+
+    double getSpeedInKilometersPerHour() {
+        return getSpeedInMetersPerSecond() * MET_PER_SEC_TO_KM_PER_H_COEFFICIENT;
+    }
+
+    double getSpeedInMilesPerHour() {
+        return getSpeedInMetersPerSecond() * MET_PER_SEC_TO_MIL_PER_H_COEFFICIENT;
     }
 }

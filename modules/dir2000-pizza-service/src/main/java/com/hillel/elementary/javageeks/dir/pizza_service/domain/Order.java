3cdcc7cc7720875812bd65f76db754642b74e9b0
Zhurylo Mikhaylo <dir2000@yjr.net>
2018-12-24 19:45:29 +0200

    pizza service dir2000 some changes

--- modules/dir2000-pizza-service/src/main/java/com/hillel/elementary/javageeks/dir/pizza_service/domain/Order.java
+++ modules/dir2000-pizza-service/src/main/java/com/hillel/elementary/javageeks/dir/pizza_service/domain/Order.java
@@ -1,4 +1,14 @@
 package com.hillel.elementary.javageeks.dir.pizza_service.domain;
 
 public class Order {
+    private long id;
+    private OrderStatus orderStatus = OrderStatus.NEW;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", orderStatus=" + orderStatus +
+                '}';
+    }
 }

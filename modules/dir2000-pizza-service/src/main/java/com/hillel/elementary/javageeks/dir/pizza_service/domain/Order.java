d25398ad9ee3391314a518ee10a4d9cd9239cbc1
Zhurylo Mikhaylo <dir2000@yjr.net>
2018-12-24 23:44:50 +0200

    pizza service dir2000 some changes

--- modules/dir2000-pizza-service/src/main/java/com/hillel/elementary/javageeks/dir/pizza_service/domain/Order.java
+++ modules/dir2000-pizza-service/src/main/java/com/hillel/elementary/javageeks/dir/pizza_service/domain/Order.java
@@ -1,8 +1,14 @@
 package com.hillel.elementary.javageeks.dir.pizza_service.domain;
 
+import java.math.BigDecimal;
+import java.util.Collection;
+
 public class Order {
     private long id;
+    private Customer customer;
+    private Collection<Pizza> pizzas;
     private OrderStatus orderStatus = OrderStatus.NEW;
+    private BigDecimal total;
 
     @Override
     public String toString() {

cf2aa498e65fcc7e5958c617b27b32c64e9d05ba
Zhurylo Mykhailo <dir2000@ukr.net>
2019-01-03 10:34:30 +0200

    jg-106 cyclic barrier dir2000 add some changes

---
+++ modules/dir2000-pizza-service/src/test/java/com/hillel/elementary/javageeks/dir/pizza_service/services/customer/SimpleCustomerServiceTest.java
@@ -0,0 +1,27 @@
+package com.hillel.elementary.javageeks.dir.pizza_service.services.customer;
+
+import com.hillel.elementary.javageeks.dir.pizza_service.repositories.customer.CustomerRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import org.mockito.Mockito;
+
+class SimpleCustomerServiceTest {
+
+  @Test
+  void getById() {
+    //given
+    CustomerRepository repository = mock(CustomerRepository.class);
+
+  }
+
+  @Test
+  void getByName() {
+  }
+
+  @Test
+  void register() {
+  }
+}

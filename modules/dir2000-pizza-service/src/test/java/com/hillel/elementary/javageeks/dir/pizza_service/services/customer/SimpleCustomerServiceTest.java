46467ed7571e433819251b3b773368be01fe1663
Zhurylo Mykhailo <dir2000@ukr.net>
2019-01-04 10:26:04 +0200

    pizza service dir2000 edit some tests

--- modules/dir2000-pizza-service/src/test/java/com/hillel/elementary/javageeks/dir/pizza_service/services/customer/SimpleCustomerServiceTest.java
+++ modules/dir2000-pizza-service/src/test/java/com/hillel/elementary/javageeks/dir/pizza_service/services/customer/SimpleCustomerServiceTest.java
@@ -1,20 +1,35 @@
 package com.hillel.elementary.javageeks.dir.pizza_service.services.customer;
 
+import com.hillel.elementary.javageeks.dir.pizza_service.domain.Customer;
 import com.hillel.elementary.javageeks.dir.pizza_service.repositories.customer.CustomerRepository;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 class SimpleCustomerServiceTest {
+  @Mock  CustomerRepository repositoryMock = mock(CustomerRepository.class);
+  @Mock  Customer customerMock = mock(Customer.class);
+  CustomerService service = new SimpleCustomerService(repositoryMock);
+
+  {
+    MockitoAnnotations.initMocks(SimpleCustomerServiceTest.class);
+  }
 
   @Test
   void getById() {
-    //given
-    CustomerRepository repository = mock(CustomerRepository.class);
+    //when
+    when(repositoryMock.findById(anyLong())).thenReturn(customerMock);
 
+    //then
+    Customer customer = service.getById(anyLong());
+    assertEquals(customerMock, customer);
   }
 
   @Test

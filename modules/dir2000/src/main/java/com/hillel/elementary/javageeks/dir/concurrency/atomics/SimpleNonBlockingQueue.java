9357d8c7370d7317bfa931520c63ddc8a8968d79
Zhurylo Mykhailo <dir2000@ukr.net>
2019-01-14 10:59:23 +0200

    atomics dir2000 - rename field "head" to "tail", add "final" modifier to "value" field of Node class

--- modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/concurrency/atomics/SimpleNonBlockingQueue.java
+++ modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/concurrency/atomics/SimpleNonBlockingQueue.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class SimpleNonBlockingQueue<T> extends AbstractQueue<T> {
-    private AtomicReference<Node<T>> head = new AtomicReference<>(null);
+    private AtomicReference<Node<T>> tail = new AtomicReference<>(null);
     private AtomicInteger size = new AtomicInteger(0);
 
     @Override
@@ -23,10 +23,10 @@ public class SimpleNonBlockingQueue<T> extends AbstractQueue<T> {
     public boolean offer(T value) {
         if (value == null) throw new IllegalArgumentException();
         while (true) {
-            Node<T> first = head.get();
-            Node<T> node = new Node<>(value, first);
+            Node<T> last = tail.get();
+            Node<T> node = new Node<>(value, last);
 
-            if (head.compareAndSet(first, node)) {
+            if (tail.compareAndSet(last, node)) {
                 size.incrementAndGet();
                 return true;
             }
@@ -36,17 +36,17 @@ public class SimpleNonBlockingQueue<T> extends AbstractQueue<T> {
     @Override
     public T poll() {
         while(true) {
-            Node<T> first = head.get();
-            if (first == null) return null;
+            Node<T> last = tail.get();
+            if (last == null) return null;
 
-            if (first.next == null) {
-                if (head.compareAndSet(first, null)) {
+            if (last.next == null) {
+                if (tail.compareAndSet(last, null)) {
                     size.decrementAndGet();
-                    return first.value;
+                    return last.value;
                 }
             } else {
-                Node<T> currNode = first;
-                Node<T> newFirst = new Node<>(first.value, null);
+                Node<T> currNode = last;
+                Node<T> newFirst = new Node<>(last.value, null);
                 Node<T> newCurrNode = newFirst;
                 T valueToReturn = currNode.next.value;
                 while (currNode.next.next != null) {
@@ -56,7 +56,7 @@ public class SimpleNonBlockingQueue<T> extends AbstractQueue<T> {
                     currNode = currNode.next;
                     valueToReturn = currNode.next.value;
                 }
-                if (head.compareAndSet(first, newFirst)) {
+                if (tail.compareAndSet(last, newFirst)) {
                     size.decrementAndGet();
                     return valueToReturn;
                 }
@@ -66,7 +66,7 @@ public class SimpleNonBlockingQueue<T> extends AbstractQueue<T> {
 
     @Override
     public T peek() {
-        Node<T> node = head.get();
+        Node<T> node = tail.get();
         if (node == null) return null;
         while (node.next != null) {
             node = node.next;
@@ -76,7 +76,7 @@ public class SimpleNonBlockingQueue<T> extends AbstractQueue<T> {
     }
 
     private static class Node<T> {
-        private T value;
+        private final T value;
         private Node<T> next;
 
         public Node(T value, Node<T> next) {

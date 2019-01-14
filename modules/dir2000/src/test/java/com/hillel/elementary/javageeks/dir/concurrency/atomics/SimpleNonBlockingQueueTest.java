f6d3229ee9413a14fb21f27787332bc25a4afbc5
Zhurylo Mykhailo <dir2000@ukr.net>
2019-01-14 14:24:23 +0200

    atomics dir2000 - improve test
    the test method has been divided into two methods - to test concurrent queue work and to test iterator
    new constants introduces instead of int literals in the test class

--- modules/dir2000/src/test/java/com/hillel/elementary/javageeks/dir/concurrency/atomics/SimpleNonBlockingQueueTest.java
+++ modules/dir2000/src/test/java/com/hillel/elementary/javageeks/dir/concurrency/atomics/SimpleNonBlockingQueueTest.java
@@ -12,13 +12,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SimpleNonBlockingQueueTest {
+  private final SimpleNonBlockingQueue<Integer> queue = new SimpleNonBlockingQueue<>();
+  private final Collection<Callable<Object>> threads = new ArrayList<>();
+  private final AtomicInteger counter = new AtomicInteger();
+  private final int THREAD_PAIRS_COUNT = 100;
+  private final int TRIES_TO_INVOKE = 100;
+  private final int MILLISECONDS_TO_AWAIT = 10000;
 
   @Test
   void shouldAddAndDeleteElementConcurrently() throws InterruptedException, ExecutionException {
-    SimpleNonBlockingQueue<Integer> queue = new SimpleNonBlockingQueue<>();
-    Collection<Callable<Object>> threads = new ArrayList<>();
-    AtomicInteger counter = new AtomicInteger();
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < THREAD_PAIRS_COUNT; i++) {
       int number = i;
       threads.add(new Callable<Object>() {
         @Override
@@ -30,7 +33,7 @@ class SimpleNonBlockingQueueTest {
       threads.add(new Callable<Object>() {
         @Override
         public Object call() throws Exception {
-          for (int j = 0; j < 10; j++) {
+          for (int j = 0; j < TRIES_TO_INVOKE; j++) {
             Integer x = queue.poll();
             if (x != null) {
               System.out.println(x);
@@ -40,8 +43,39 @@ class SimpleNonBlockingQueueTest {
           return null;
         }
       });
+    }
+
+    ExecutorService service = Executors.newFixedThreadPool(THREAD_PAIRS_COUNT);
+    List<Future<Object>> futures = service.invokeAll(threads);
+    service.awaitTermination(MILLISECONDS_TO_AWAIT, TimeUnit.MILLISECONDS);
+    assertThat(counter.get()).isEqualTo(THREAD_PAIRS_COUNT);
+  }
+
+  @Test
+  void shouldIterateOverQueueConcurrently() throws InterruptedException, ExecutionException {
+    for (int i = 0; i < THREAD_PAIRS_COUNT; i++) {
+      int number = i;
+      threads.add(new Callable<Object>() {
+        @Override
+        public Object call() throws Exception {
+          queue.add(number);
+          return null;
+        }
+      });
+      threads.add(new Callable<Object>() {
+        @Override
+        public Object call() throws Exception {
+          for (int j = 0; j < TRIES_TO_INVOKE; j++) {
+            Integer x = queue.poll();
+            if (x != null) {
+              counter.incrementAndGet();
+            }
+          }
+          return null;
+        }
+      });
       //to check that iterators don't fail and do not influence on usual queue work
-      for (int j = 0; j < 10; j++) {
+      for (int j = 0; j < TRIES_TO_INVOKE; j++) {
         threads.add(new Callable<Object>() {
           @Override
           public Object call() throws Exception {
@@ -55,9 +89,9 @@ class SimpleNonBlockingQueueTest {
       }
     }
 
-    ExecutorService service = Executors.newFixedThreadPool(100);
+    ExecutorService service = Executors.newFixedThreadPool(THREAD_PAIRS_COUNT);
     List<Future<Object>> futures = service.invokeAll(threads);
-    service.awaitTermination(10000, TimeUnit.MILLISECONDS);
-    assertThat(counter.get()).isEqualTo(100);
+    service.awaitTermination(MILLISECONDS_TO_AWAIT, TimeUnit.MILLISECONDS);
+    assertThat(counter.get()).isEqualTo(THREAD_PAIRS_COUNT);
   }
 }

b19b9bd681dc71c680af18e26cc60a10a8fe3030
Zhurylo Mykhailo <dir2000@ukr.net>
2019-01-31 11:00:53 +0200

    pizza service dir2000 - try to use streams in homeworks

--- modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/homework7/bizzare_sorting_of_words/BizarreComparator.java
+++ modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/homework7/bizzare_sorting_of_words/BizarreComparator.java
@@ -1,8 +1,12 @@
 package com.hillel.elementary.javageeks.dir.homework7.bizzare_sorting_of_words;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class BizarreComparator implements Comparator<String> {
   private static final Pattern PATTERN = Pattern.compile("[aeiouyAEIOUY]");
@@ -10,10 +14,30 @@ public class BizarreComparator implements Comparator<String> {
   public static int vowelLettersCount(String word) {
     Matcher matcher = PATTERN.matcher(word);
     int counter = 0;
+
+    List<String> list = new ArrayList<>();
     while (matcher.find()) {
+      list.add(word.substring(matcher.start(), matcher.end()));
       counter++;
     }
+    String[] array = list.toArray(new String[list.size()]);
+    System.out.println("Word: " + word);
+    System.out.println("Ususal result: " + Arrays.deepToString(array));
+
+    int streamCounter = vowelLettersCountStream(word);
+    System.out.println("Stream check: " + counter + " == " + streamCounter + " = " + (counter == streamCounter) + "\n");
+
     return counter;
+  }
+
+  private static int vowelLettersCountStream(String word) {
+    Pattern antiPattern = Pattern.compile("[^aeiouyAEIOUY]");
+    Stream<String> stream = antiPattern.splitAsStream(word);
+
+    Object[] array = stream.toArray();
+    System.out.println("Stream result: " + Arrays.deepToString(array));
+    int streamCounter = array.length;
+    return streamCounter;
   }
 
   @Override

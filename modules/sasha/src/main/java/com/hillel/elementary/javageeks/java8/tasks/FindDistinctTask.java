fa88064119f5e3dafafc069fb7700b6e2dc260e3
sasha <ostoleksandr1@gmail.com>
2019-02-02 19:05:48 +0200

    Sasha added tasks

---
+++ modules/sasha/src/main/java/com/hillel/elementary/javageeks/java8/tasks/FindDistinctTask.java
@@ -0,0 +1,77 @@
+package com.hillel.elementary.javageeks.java8.tasks;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class FindDistinctTask {
+
+    // Вернуть список с уникальными задачами (доп. сложность - с уникальными именами)
+    public static List<Task> allDistinctTasks(List<Task> tasks) {
+        List<Task>taskList = tasks.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        return taskList;
+    }
+
+    // Найти 5 первых (по дате создания) задач по чтению, вернуть имя задачи
+    public static List<String> top5ReadingTaskNames(List<Task> tasks) {
+
+        throw new UnsupportedOperationException();
+    }
+
+    // Посчитать количество задач указанного типа
+    public static Long countAllTasksByType(List<Task> tasks, TaskType type) {
+        Long longA = tasks.stream()
+                .map(task -> task.getType())
+                .filter(TaskType-> TaskType.equals(type))
+                .count();
+        return longA;
+    }
+
+    // Вернуть все уникальные теги задач
+    public static List<String> getAllDistinctTags(List<Task> tasks) {
+        List<String>stringList = tasks.stream()
+                .flatMap( task -> task.getTags().stream())
+                .distinct()
+                .collect(Collectors.toList());
+
+        return stringList;
+    }
+
+    // Проверить у всех ли задач по чтению есть тег "Books"
+    public static Boolean isAllReadingTasksWithTagBooks(List<Task> tasks) {
+        return tasks.stream()
+                .filter(task -> task.getType().equals(TaskType.READING))
+                .allMatch(task ->task.getTags().contains("Books"));
+    }
+
+    //Сконкатенировать все названия задач в одну надпись через "***"
+    public static String joinAllTaskTitles(List<Task> tasks) {
+        Optional<String>stringOptional = tasks.stream()
+                .map(task -> task.getName())
+                .reduce((s1,s2)-> s1 + "***" + s2);
+        return stringOptional.get();
+    }
+
+    // Вернуть строку с числами, перечисленными через запятую на основе заданного списка целых чисел.
+    // Каждому элементу должна предшествовать буква «e», если число четное, и буква «o», если число нечетное.
+    // Например, если список ввода (3,44), вывод должен быть 'o3, e44'.
+    public static String evensAndOdds(String numbers) {
+//        Arrays.stream(numbers.split(","))
+//                .map()
+        throw new UnsupportedOperationException();
+    }
+
+    //Вернуть список всех строк, которые начинаются с буквы «а» и имеют ровно 3 буквы
+    public static List<String> searchForThreeLetterWordsStartingWithA(List<String> list) {
+        List<String>stringList = list.stream()
+                .filter(s -> s.length() == 3)
+                .filter(s -> s.startsWith("a"))
+                .collect(Collectors.toList());
+
+        return stringList;
+    }
+
+}
---
+++ modules/sasha/src/main/java/com/hillel/elementary/javageeks/java8/tasks/TaskType.java
@@ -0,0 +1,7 @@
+package com.hillel.elementary.javageeks.java8.tasks;
+
+enum TaskType {
+    READING,
+    WRITING,
+    SPEAKING
+}

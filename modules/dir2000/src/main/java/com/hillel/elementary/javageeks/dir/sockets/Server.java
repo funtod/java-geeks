7dac0ffc3fb910795f0d60741da14665b0d1cf65
Zhurylo Mikhaylo <dir2000@yjr.net>
2019-02-06 23:49:39 +0200

    sockets dir2000 modify Server class

--- modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/sockets/Server.java
+++ modules/dir2000/src/main/java/com/hillel/elementary/javageeks/dir/sockets/Server.java
@@ -1,104 +1,125 @@
 package com.hillel.elementary.javageeks.dir.sockets;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Iterator;
 import java.util.Scanner;
-import java.util.concurrent.*;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
-  ConcurrentHashMap<Socket, PrintWriter> clientOutputStreams;
-  ExecutorService executor;
+    public static final int PORT = 5000;
+    CopyOnWriteArraySet<Socket> clientSockets;
+    ExecutorService executor;
 
-  public Server() {
-    clientOutputStreams = new ConcurrentHashMap<>();
-    executor = Executors.newCachedThreadPool();
-  }
-
-  public static void main(String[] args) {
-    new Server().go();
-  }
-
-  public void go() {
-    executor.execute(new ServerTask());
-
-    try (Scanner scanner = new Scanner(System.in)) {
-      String prompt = "Enter \"shutdown\" to stop the server...";
-      System.out.println(prompt);
-      while (scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        if (line.equalsIgnoreCase("shutdown")) {
-          break;
-        } else {
-          System.out.println(prompt);
-        }
-      }
-    }
-    executor.shutdownNow();
-    System.out.println("Shutdown command accepted.");
-  }
-
-
-  public void tellEveryone(String message) {
-    Iterator<PrintWriter> it = clientOutputStreams.values().iterator();
-    while (it.hasNext()) {
-      try {
-        PrintWriter writer = it.next();
-        writer.println(message);
-        writer.flush();
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    }
-  }
-
-  private class ServerTask implements Runnable {
-    @Override
-    public void run() {
-      try (ServerSocket serverSock = new ServerSocket(5000);) {
-        while (!Thread.currentThread().isInterrupted()) {
-          Socket clientSocket = serverSock.accept();
-          PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
-          clientOutputStreams.put(clientSocket, writer);
-
-          executor.execute(new ClientHandler(clientSocket));
-          System.out.println("got a connection");
-        }
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    }
-  }
-
-  private class ClientHandler implements Runnable {
-    Socket clientSocket;
-
-    public ClientHandler(Socket argClientSocket) {
-      try {
-        clientSocket = argClientSocket;
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
+    public Server() {
+        clientSockets = new CopyOnWriteArraySet<>();
+        executor = Executors.newCachedThreadPool();
     }
 
-    public void run() {
-      String message;
-      try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
-        while ((message = reader.readLine()) != null) {
-          System.out.println("read " + message);
-          if (message.equalsIgnoreCase("bye")) {
-            clientSocket.close();
-            clientOutputStreams.remove(clientSocket);
+    public static void main(String[] args) {
+        new Server().go();
+    }
+
+    public void go() {
+        ServerSocket serverSock;
+        try {
+            serverSock = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
             return;
-          }
-          tellEveryone(message);
         }
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
+        try (Scanner scanner = new Scanner(System.in)) {
+            Thread serverThread = new Thread(new ServerTask(serverSock));
+            serverThread.start();
+
+            String prompt = "Enter \"shutdown\" to stop the server...";
+            System.out.println(prompt);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.equalsIgnoreCase("shutdown")) {
+                    break;
+                } else {
+                    System.out.println(prompt);
+                }
+            }
+            System.out.println("Shutdown command accepted.");
+            serverSock.close();
+            serverThread.join();
+            for (Socket socket : clientSockets) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-  }
+
+    public void tellEveryone(String message) {
+        Iterator<Socket> it = clientSockets.iterator();
+        while (it.hasNext()) {
+            try {
+                PrintWriter writer = new PrintWriter(it.next().getOutputStream());
+                writer.println(message);
+                writer.flush();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private class ServerTask implements Runnable {
+        ServerSocket serverSock;
+
+        public ServerTask(ServerSocket serverSock) {
+            this.serverSock = serverSock;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Socket clientSocket = serverSock.accept();
+                    clientSockets.add(clientSocket);
+                    executor.execute(new ClientHandler(clientSocket));
+                    System.out.println("got a connection");
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private class ClientHandler implements Runnable {
+        Socket clientSocket;
+
+        public ClientHandler(Socket argClientSocket) {
+            clientSocket = argClientSocket;
+        }
+
+        public void run() {
+            String message;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    if (message.equalsIgnoreCase("bye")) {
+                        clientSocket.close();
+                        clientSockets.remove(clientSocket);
+                        return;
+                    }
+                    tellEveryone(message);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
 }

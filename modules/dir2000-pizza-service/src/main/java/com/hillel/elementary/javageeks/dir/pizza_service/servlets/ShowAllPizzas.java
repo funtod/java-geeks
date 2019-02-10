5072b614a29341cd3b1c8946e7d58f4d2f7b77ac
Zhurylo Mikhaylo <dir2000@yjr.net>
2019-02-10 19:57:02 +0200

    pizza-service dir2000 add index.html and CreateOrder, ShowAllPizzas servlets

---
+++ modules/dir2000-pizza-service/src/main/java/com/hillel/elementary/javageeks/dir/pizza_service/servlets/ShowAllPizzas.java
@@ -0,0 +1,20 @@
+package com.hillel.elementary.javageeks.dir.pizza_service.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ShowAllPizzas extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("Show all pizzas");
+        out.close();
+//        RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/add.jsp");
+//        requestDispatcher.forward(req, resp);
+    }
+}
